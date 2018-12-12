82c4052dbbe6df7ef1b4e3119e113f9f2f5c78aa
maria <msmariaulfa33@gmail.com>
2018-12-12 11:53:31 +0700

    submit

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// const sleep = require('sleep');
 
-function readFilePromise() {
+function readFilePromise(file) {
   // psst, the promise should be around here...
+  const filePromise = new Promise (function(resolve, reject) {
+    fs.readFile (file, 'utf8', function(err, data) {
+      if (err) {
+        reject(err)
+      }else{
+        resolve(data)
+      }
+    })
+  })
+  return filePromise
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName)
+    .then(function(dataParent) {
+      readFilePromise(childrenFileName)
+        .then(function(dataChildern){
+          let parent = JSON.parse(dataParent)
+          let child = JSON.parse(dataChildern)
+          for(let i = 0; i < parent.length; i++) {
+            let childern = []
+            for(let j = 0; j < child.length; j++) {
+              if(child[j].family == parent[i].last_name) {
+                childern.push(child[j].full_name)
+              }
+            }
+            parent[i].childern = childern
+          }
+          console.log(parent)
+        })
+    })
+    .catch (function(err) {
+      console.log('Terjadi error pada proses pembacaan data.',err)
+    })
+
+
 }
 
-matchParentsWithChildrens('./parents.json', './childrens.json');
+// matchParentsWithChildrens('./parents.json', './childrens.json');
 console.log("Notification : Data sedang diproses !");
 
 // for Release 2

23e7abba09436ddb0827d4f7c018ea0b96bbb810
patriagani <patria.gani@gmail.com>
2018-12-12 11:32:07 +0700

    promise multiple file done

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const fs = require('fs');
-var sleep = require('sleep');
+const sleep = require('sleep');
 
-function readFilePromise() {
-  // psst, the promise should be around here...
+function readFilePromise(dataPath) {
+  sleep.sleep(2)
+  let promise = new Promise (function (resolve, reject) {
+    let data = fs.readFile(dataPath, 'utf8', function(err, data) {
+      if (err) {
+        reject(err)
+      }
+      else {
+        resolve(data)
+      }
+    })
+  })
+  return promise
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
-  // your code here... (p.s. readFilePromise function(s) should be around here..)
+  let parentData = null
+  let childrenData = null
+  readFilePromise(parentFileName)
+    .then(function(parent) {
+      parentData = JSON.parse(parent)
+      return readFilePromise(childrenFileName)
+    })
+    .then(function(children) {
+      childrenData = JSON.parse(children)
+
+      for (let i = 0; i < parentData.length; i++) {
+        parentData[i]["childrens"] = []
+      }
+      for (let i = 0; i < parentData.length; i++) {
+        for (let j = 0; j < childrenData.length; j++) {
+          if (childrenData[j].family === parentData[i].last_name) {
+            parentData[i].childrens.push(childrenData[j].full_name)
+          }
+        }
+      }
+      console.log(parentData);
+    })
+    .catch(function(err) {
+      console.log(`Terjadi error pada proses pembacaan data`);
+      console.log(err);
+    })
+
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');

fc7afc14ecf68280bdba97c0bad6095cfb3944d9
ChristianHS23 <christian.sihotang23@gmail.com>
2018-12-12 15:15:30 +0700

    don promise

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function readFilePromise() {
+function readFilePromise(pathFile) {
   // psst, the promise should be around here...
+  return new Promise ((resolve, reject)=> {
+    fs.readFile(pathFile,'utf8', (err,data)=> {
+      if(err) reject(err)
+      else resolve(data)
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
+  let tempParent = []
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  
+  readFilePromise(parentFileName).then((dataParent)=> {
+    sleep.sleep(5)
+    tempParent = JSON.parse(dataParent)
+    return readFilePromise(childrenFileName)
+  }).then((dataChildren)=> {
+    dataChildren = JSON.parse(dataChildren)
+    for(let i = 0;i < tempParent.length; i++) {
+      tempParent[i].children = []
+      for(let j = 0; j < dataChildren.length; j++) {
+        if(tempParent[i].last_name == dataChildren[j].family) {
+          tempParent[i].children.push(dataChildren[j].full_name)
+        }
+      }
+    }
+    console.log(tempParent);
+  }).catch((err)=> {
+    console.log(err);
+  })
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
